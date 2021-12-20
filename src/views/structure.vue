b89907e430028c75f55aa452d47b95ed1c111f6a
kondwani13 <sakalakhondwani1@gmail.com>
2021-12-20 04:55:26 -0800

    explore

--- src/views/structure.vue
+++ src/views/structure.vue
@@ -2,7 +2,7 @@
     
         <section class="bg-white dark:bg-gray-900">
             <div class="container px-6 py-10 mx-auto">
-                <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">explore our <br> awesome <span class="text-blue-500">Components</span></h1>
+                <h1 class="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">explore our <br> effective <span class="text-blue-500">Techniques</span></h1>
                 
                 <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
                     <div class="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
